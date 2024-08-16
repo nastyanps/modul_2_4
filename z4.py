27758753c867fb9c47e4f3c3ddf766a153b5468f
nastyanps <yfcnz23062005@mail.ru>
2024-08-16 22:30:55 +0300

    Add files via upload

---
+++ z4.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_prime = bool
+
+for i in numbers:
+    if i != 1:
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+                break
+            if i % j != 0:
+                is_prime = True
+        if is_prime:
+            primes.append(i)
+        else:
+            not_primes.append(i)
+print('Простые: ', primes)
+print('Составные: ', not_primes)
